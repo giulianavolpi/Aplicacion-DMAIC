ef25831050a4c4b1158b95c076bd4a33d2517d56
giulianavolpi <g.volpi@uniandes.edu.co>
2023-05-24 15:55:29 -0500

    Recuperacion todo

---
+++ model.py
@@ -0,0 +1,78 @@
+﻿"""
+Es la lógica del programa
+Donde se calculan los porcentajes del indicador
+ """
+#=========================================================
+# Llenamos el catalgo con la info
+#=========================================================
+
+def crear_catalogo():
+    archivo = open ("base_datos.csv", 'r', encoding='utf-8')
+    datos=[]
+    encabezados=archivo.readline().strip().split (",")
+
+    for linea in archivo:
+        informacion = linea.strip().split(",")
+        
+        empresa={
+            "id_empresa":informacion [0],
+            encabezados [1]:informacion [1], 
+            encabezados [2]:int(informacion [2]),
+            encabezados [3]:int(informacion [3]),
+            encabezados [4]:int(informacion [4]),
+            encabezados [5]:float(informacion [5])}
+     
+        datos.append(empresa)
+        
+    archivo.close()
+    return datos
+
+
+#=========================================================
+#  R. Intriducción al porgrama 
+#=========================================================
+def intro():
+    intro = "A partir de las estadísticas del sector azucarero colombiano y de los datos brindados por el ingenio Riopaila y Castilla se realizó un programa para calcular y analizar el indicador de desempeño frente a distintas empresas. \nCon los valores se distintas empresas se puede calcular el porcentaje \nde desperdicio o para obtener un porcentaje desead se calcuan las cantidades de máquinas y de operarios necesariaos.\n Se llegó a la conclusión que el la mejor distribución entre caña larga y caña mecanizada es la siguiente: \n"
+    porc = " Caña larga: 35%\n"
+    porc2 = "Caña mecanizada : 65%"
+    
+    return intro, porc, porc2 
+
+#=========================================================
+# R. Análisis Indicador 
+#=========================================================      
+def analisis_indic():
+    
+    a = "El idicador de desempeño calcula la capacidad de desperdicio de caña por vagones al distribuir la caña entre un contre manual y uno mecanizado\n"
+    b = "El cálculo del indicador es: Desperdicio = 1-(Capacidad de caña larga por vagón / Capacidad de caña mecanizada por vagón)\n"
+    
+    return a,b 
+
+#=========================================================
+# R. Cálculo de porcentaje desperdiciado
+#========================================================= 
+def porcent_desperdicio(larga, mecanizada, nombre, catalogo):
+   
+    desp = 1- (larga/mecanizada)
+    for empresa in catalogo:
+        if empresa["nombre"] == nombre:
+            cantidad = empresa["toneladas_cana"]
+            cantidad = cantidad * desp
+   
+    return desp, cantidad
+
+#==================================================================
+# R. Requerimientos en máquinas y operarios para desperdicio mínimo
+#==================================================================
+def para_porcent(nombre, catalogo):
+
+    for empresa in catalogo:
+        if empresa["nombre"] == nombre:
+            cantidad = empresa["toneladas_cana"]
+    larga = cantidad * 0.35
+    mecan = cantidad * 0.65
+
+    personas = round((larga/5.5),0)
+    maquinas = round((mecan/160),0)
+    
+    return personas, maquinas 
---
+++ view.py
@@ -0,0 +1,116 @@
+﻿"""
+La vista se encarga de la interacción con el usuario
+Presenta el menu de opciones y por cada seleccion
+se hace la solicitud al controlador para ejecutar la
+operación solicitada
+"""
+
+#from DISClib.ADT import list as lt
+import model as mod
+
+#=========================================================
+# R. Intriducción al porgrama 
+#=========================================================
+def intro():
+    lista=mod.intro()
+    return lista
+#=========================================================
+# R. Análisis Indicador 
+#=========================================================
+def analisis_indic():
+    lista=mod.analisis_indic()
+    return lista
+
+#=========================================================
+# R. Cálculo de porcentaje desperdiciado
+#=========================================================
+def porcent_desperdicio(larga, mecanizada, nombre, catalogo):
+    lista=mod.porcent_desperdicio(larga, mecanizada, nombre, catalogo)
+    return lista
+
+#===================================================================
+# R. Requerimientos en máquinas y operarios para desperdicio mínimo
+#===================================================================
+def para_porcent(nombre,catalogo):
+    lista=mod.para_porcent(nombre,catalogo)
+    return lista
+
+
+#=========================================================
+# MENU
+#=========================================================
+def printMenu():
+    print("Bienvenido")
+    print("0- Cargar datos")
+    print("1- Introducción Aplicación")
+    print("2- Análisis Indicador")
+    print("3- Cálculo de porcentajes de desperdicio")
+    print("4- Requisitos para desperdicio mínimo")
+    print("11- Salir ")
+
+
+#=========================================================
+# Menu principal
+#=========================================================
+while True:
+    printMenu()
+    inputs = input('Seleccione una opción para continuar\n')  
+    
+    if int(inputs[0]) == 0:
+        #cargar el archivo
+        print("Cargando información de los archivos ....\n")
+        catalogo= mod.crear_catalogo()
+        print('Datos cargados correctamente')
+            
+    if int(inputs[0]) == 1:
+        
+        print ("Bienvenido a la Aplicación DMAIC")
+        print ("Se calculará el desperdicio en el manejo de caña larga y caña mecanizada en los vagones")
+        resp=intro()
+        print (resp[0])
+        print (resp[1])
+        print (resp[2])
+     
+
+
+    if int(inputs[0]) == 2:
+        
+        print ("Análisis del Indicador")
+        resp = analisis_indic()
+        print(resp[0]) 
+        print(resp[1])
+
+
+    if int(inputs[0]) == 3:
+        #lista_imprimir = lt.newList()
+        nombre=input("Digíte el nombre de la empresa: ")
+        larga=int(input("Digíte la capacidad de caña larga de su vagón: "))
+        mecanizada=int(input("Digíte la capacidad de caña mecanizada de su vagón: "))
+        resp = porcent_desperdicio(larga, mecanizada, nombre, catalogo)
+        
+        desperdicio = resp[0]
+        cantidad = resp[1]
+        #lt.addLast(lista_imprimir,desperdicio)
+        #lt.addLast(lista_imprimir,cantidad)
+        print("El porcentaje de desperdicio de la empresa: " + nombre + " es de: ")
+        print (desperdicio)
+        print (cantidad)
+        #print(tabulate(lista_imprimir,headers=["Porcentaje Desperdicio","Cantidad de Caña Desperdiciada"],tablefmt="fancy_grid",maxcolwidths=[10,10]))
+
+    if int(inputs[0]) == 4:
+        #lista_imprimir = lt.newList()
+        nombre=input("Digíte el nombre de la empresa: ")
+        resp = para_porcent(nombre,catalogo)
+
+        personas = resp[0]
+        maquinas = resp[1]
+
+        #lt.addLast(lista_imprimir,personas)
+        #lt.addLast(lista_imprimir,maquinas)
+        print("Para obenter un porcentaje de desperdicio deseado, siguiendo el indicador.")
+        print("Los requisitos para la empresa " + nombre + " son los siguientes: ")
+        print ("Cantidad de personas: " + str(personas))
+        print ("Cantidad de máquinas: " + str(maquinas))
+        #print (tabulate(lista_imprimir, headers=["Operarios","Máquinas"],tablefmt="fancy_grid",maxcolwidths=[10,10]))
+printMenu()
+
